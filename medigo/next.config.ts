def6d9c756c3b8221aa7e8466329470150319e96
TanujGamerHP <tanujprakash579@gmail.com>
2026-07-08 16:46:22 +0530

    Fix syntax error in next.config.ts

--- medigo/next.config.ts
+++ medigo/next.config.ts
@@ -5,6 +5,7 @@ const nextConfig: NextConfig = {
   transpilePackages: ["three", "@react-three/fiber", "@react-three/drei"],
   images: {
     formats: ["image/avif", "image/webp"],
+  },
 };
 
 export default nextConfig;
